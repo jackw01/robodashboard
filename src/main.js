90722b56eb5f34925b463d876e4481a5b1f79113
jackw01 <jackw01@users.noreply.github.com>
2018-11-02 23:03:22 -0700

    set up project

---
+++ src/main.js
@@ -0,0 +1,2 @@
+// robodashboard - Node.js web dashboard for displaying data from and controlling teleoperated robots
+// Copyright 2018 jackw01. Released under the MIT License (see LICENSE for details).
