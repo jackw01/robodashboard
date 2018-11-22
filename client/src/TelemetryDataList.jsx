55e014400260928086a2d0a2aa47822db7922aff
jackw01 <jackw01@users.noreply.github.com>
2018-11-21 23:07:16 -0800

    working on better state management

--- client/src/TelemetryDataList.jsx
+++ client/src/TelemetryDataList.jsx
@@ -34,7 +34,7 @@ const SortableList = SortableContainer(({ items, dataPoints, visible }) => {
 class TelemetryDataList extends Component {
   constructor(props) {
     super(props);
-    this.state = { visible: JSON.parse(localStorage.getItem('telemetryDataListShowAll')) || false, items: [] };
+    this.state = { visible: JSON.parse(localStorage.getItem('telemetryDataListShowAll')) || false, items: [], visibility: JSON.parse(localStorage.getItem('telemetryDataListVisibility')) || {}, visibilityToggle: JSON.parse(localStorage.getItem('telemetryDataListVisibilityToggle')) || false };
     telemetryClient.on('ready', () => {
       this.setState({
         items: JSON.parse(localStorage.getItem('telemetryDataListOrder')) || Object.keys(telemetryClient.dataPoints),
@@ -49,6 +49,15 @@ class TelemetryDataList extends Component {
     });
   };
 
+  toggleAllGraphs() {
+    this.setState((state) => {
+      const newToggleState = !state.visibilityToggle;
+      const newVisibility = state.visibility;
+      Object.keys(newVisibility).forEach((k) => { newVisibility[k] = newToggleState; });
+      return { visibility: newVisibility, visibilityToggle: newToggleState };
+    });
+  };
+
   onSortEnd = ({oldIndex, newIndex}) => {
     this.setState({ items: arrayMove(this.state.items, oldIndex, newIndex) }, () => {
       localStorage.setItem('telemetryDataListOrder', JSON.stringify(this.state.items));
@@ -56,6 +65,7 @@ class TelemetryDataList extends Component {
   };
 
   render() {
+    /*
     return (
       <Container className='telemetry-data-list'>
         <Row>
@@ -64,6 +74,22 @@ class TelemetryDataList extends Component {
             <br/>
             <Button color="primary" onClick={this.toggleGraphs.bind(this)}
               active={this.state.visible}>Graph All</Button>
+          </Col>
+        </Row>
+        <hr/>
+        <SortableList items={this.state.items} dataPoints={this.state.dataPoints}
+          visible={this.state.visible} key={this.state.visible} onSortEnd={this.onSortEnd}
+          pressDelay={200} lockToContainerEdges={true} lockAxis='y'/>
+      </Container>
+    );*/
+    return (
+      <Container className='telemetry-data-list'>
+        <Row>
+          <Col>
+            <span className='telemetry-data-list-title'>Telemetry</span>
+            <br/>
+            <Button color="primary" onClick={this.toggleAllGraphs.bind(this)}
+              active={this.state.visibilityToggle}>Toggle All</Button>
           </Col>
         </Row>
         <hr/>
