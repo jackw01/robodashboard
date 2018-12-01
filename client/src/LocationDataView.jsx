dd1b13a09eae256b4ed58471f27e31e81b645cc4
jackw01 <jackw01@users.noreply.github.com>
2018-11-30 23:05:55 -0800

    make graph resize aware and center gridlines

--- client/src/LocationDataView.jsx
+++ client/src/LocationDataView.jsx
@@ -2,6 +2,7 @@
 // Copyright 2018 jackw01. Released under the MIT License (see LICENSE for details).
 
 import React, { Component } from 'react';
+import ResizeAware from 'react-resize-aware';
 import { Card, CardBody, CardTitle, Button, ButtonGroup } from 'reactstrap';
 import { FlexibleXYPlot, XAxis, YAxis, Hint, HorizontalGridLines, VerticalGridLines, LineSeries } from 'react-vis';
 
@@ -15,30 +16,16 @@ class LocationDataView extends Component {
     super(props);
     this.state = {
       width: 0,
+      height: 0,
     };
-
-    window.addEventListener('resize', () => {
-
-    });
 
     telemetryClient.on('ready', () => {
 
     });
   }
 
-  updateDimensions() {
-    this.setState({
-      width: this.container.offsetWidth,
-      height: this.container.offsetHeight,
-    });
-  }
-
-  componentDidMount() {
-    window.addEventListener("resize", this.updateDimensions);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.updateDimensions);
+  handleResize({ width, height }) {
+    this.setState({ width, height });
   }
 
   render() {
@@ -46,16 +33,16 @@ class LocationDataView extends Component {
       <Card className='data-view location-data-view'>
         <CardBody>
           <CardTitle>Location</CardTitle>
-          <div className='plot-flexible-container'>
+          <ResizeAware className='plot-flexible-container' onlyEvent onResize={this.handleResize.bind(this)}>
             <FlexibleXYPlot height={400} animation={false} xDomain={[-100, 100]} yDomain={[-100, 100]}
               margin={{ left: 0, right: 0, top: 1, bottom: 1 }} dontCheckIfEmpty>
               <HorizontalGridLines style={styles.gridLines}/>
               <VerticalGridLines style={styles.gridLines}/>
               <LineSeries key='t' data={[{ x: 0, y: 0 }]} color={colors.primary}/>
               <XAxis top={200} style={styles.axes}/>
-              <YAxis left={200} style={styles.axes}/>
+              <YAxis left={this.state.width / 2} style={styles.axes}/>
             </FlexibleXYPlot>
-          </div>
+          </ResizeAware>
         </CardBody>
       </Card>
     );
